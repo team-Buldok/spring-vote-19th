a44c19934ca724790ed822661fafc5f845923abc
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-06-14 17:25:54 +0900

    Fix: response에서 count 제거

--- src/main/java/buldog/vote/dto/BaseResponse.java
+++ src/main/java/buldog/vote/dto/BaseResponse.java
@@ -9,12 +9,10 @@ public class BaseResponse<T> {
     private final HttpStatus httpStatus;
     private final String message;
     private final T value;
-    private final int count;
 
-    public BaseResponse(HttpStatus httpStatus, String message, T value, int count) {
+    public BaseResponse(HttpStatus httpStatus, String message, T value) {
         this.httpStatus = httpStatus;
         this.message = message;
         this.value = value;
-        this.count = count;
     }
 }
