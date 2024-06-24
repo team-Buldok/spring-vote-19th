7c36ead2b63c4b90bc185c58bab70cce97a90efc
Jinu Yi <dionisos198@naver.com>
2024-06-24 22:47:48 +0900

    Refactor: cors 전체 허용으로 다시 수정

--- src/main/java/buldog/vote/common/config/WebConfig.java
+++ src/main/java/buldog/vote/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
-                .allowedOrigins("http://localhost:3000","http://localhost:3003") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
+                .allowedOrigins("**") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
                 .allowedMethods("GET", "POST", "PUT", "PATCH",
                         "DELETE", "OPTIONS") // cors를 허용할 method + DELETE 추가
                 .allowedHeaders("Content-Type", "Authorization","accessToken","refreshToken")
